e8ba044278bc3fcf9a0129c85c6a47c9a2453769
Boris Ushakov <inlvert@gmail.com>
2024-10-15 23:06:35 +0300

    add cors

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = require("./routers");
 const basicErrorHendler = require('./middlewares/errors/basic');
 const tokenErrorHendler = require('./middlewares/errors/token')
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
