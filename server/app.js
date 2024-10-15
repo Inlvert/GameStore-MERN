e4d11b7fe087e6d4f97a4f77967a491a9bb0c5cd
Boris Ushakov <inlvert@gmail.com>
2024-10-15 23:00:47 +0300

    add token error

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = require("./routers");
-const basicErrorHendler = require('./middlewares/errors/basic')
+const basicErrorHendler = require('./middlewares/errors/basic');
+const tokenErrorHendler = require('./middlewares/errors/token')
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
 app.use(basicErrorHendler);
+app.use(tokenErrorHendler);
 
 module.exports = app;
