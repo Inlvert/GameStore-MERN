a282d3e30e2b949e9212256d4cc4451fd9dc824f
Boris Ushakov <inlvert@gmail.com>
2024-10-30 14:10:37 +0200

    add routing for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/Home";
+import { useDispatch } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
+import RegistrationPage from "./pages/Registration";
+import CONSTANTS from "./constants";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { refresh } from "./redux/slices/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const refreshTokenFromCookies = Cookies.get(CONSTANTS.REFRESH_TOKEN);
+
+    if(refreshTokenFromCookies) {
+      dispatch(refresh(refreshTokenFromCookies))
+    }
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/registration" component={RegistrationPage} />
+      </Switch>
     </div>
   );
 }
