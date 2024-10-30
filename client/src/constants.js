47ad6e4185007446c889796a542622e15dbbe9f9
Boris Ushakov <inlvert@gmail.com>
2024-10-30 14:11:20 +0200

    add constants to file

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,7 @@
 const CONSTANTS = {
-  PUBLIC_URL: 'http://localhost:3000/'
+  PUBLIC_URL: "http://localhost:3000/",
+  HTTP_SERVER_URL: "http://localhost:5000/",
+  REFRESH_TOKEN: "refreshToken",
 };
 
 export default CONSTANTS;
