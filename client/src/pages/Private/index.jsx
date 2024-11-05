3b77f709b46f19dddd270f412ae974da72adba53
Boris Ushakov <inlvert@gmail.com>
2024-11-06 00:13:35 +0200

    add PrivatePage

---
+++ client/src/pages/Private/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const PrivatePage = (props) => {
+  return (
+    <div style={{ backgroundColor: "#242424", height: "100vh" }}>
+      <Header />
+      <h1 style={{ color: "white" }}>PrivatePage</h1>
+    </div>
+  );
+};
+
+export default PrivatePage;
