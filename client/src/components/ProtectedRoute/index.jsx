9475630f4fc66e25073801e32367e1266b8abbd3
Boris Ushakov <inlvert@gmail.com>
2024-11-06 00:14:26 +0200

    add ProtectedRoute

---
+++ client/src/components/ProtectedRoute/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route } from "react-router-dom/cjs/react-router-dom.min";
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  return (
+    <Route {...rest} render={(props) => isAuthenticated ? <Component {...props} /> : <Redirect to="/login" /> }
+    />
+  );
+};
+
+export default ProtectedRoute;
