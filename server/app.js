1b0c1b49dc65d279154f192e64642ca692726b02
Boris Ushakov <inlvert@gmail.com>
2025-01-23 16:48:39 +0200

    add

--- server/app.js
+++ server/app.js
@@ -6,11 +6,21 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: 'https://game-store-mern.vercel.app/',  // Replace with your frontend's URL
+  methods: 'GET,POST,PUT,DELETE', // You can customize the methods allowed
+  credentials: true  // If you're using cookies or auth tokens that need credentials
+}));
 
 app.use(express.json());
 app.use(router);
 app.use(basicErrorHendler);
 app.use(tokenErrorHendler);
+
+app.get('/', (_req, res) => {
+  res.json("Hello")
+})
 
 module.exports = app;
