cd053f50486cae0ee8652428f38ad79161c4aea7
Boris Ushakov <inlvert@gmail.com>
2025-01-23 16:27:07 +0200

    add

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
-const SERVER_URL = 'game-store-mern.vercel.app/';
+const SERVER_URL = 'https://game-store-mern-wx1r.vercel.app/';
 
 const CONSTANTS = {
-  PUBLIC_URL: "http://localhost:3000/",
+  PUBLIC_URL: "https://game-store-mern.vercel.app/",
   HTTP_SERVER_URL: `https://${SERVER_URL}`,
   REFRESH_TOKEN: "refreshToken",
 };
