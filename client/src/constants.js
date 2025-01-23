c12af86e1c7140c63c252c1acac41695aebe10de
Boris Ushakov <inlvert@gmail.com>
2025-01-23 15:31:35 +0200

    add cahnge to constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,8 @@
+const SERVER_URL = 'game-store-mern.vercel.app/';
+
 const CONSTANTS = {
   PUBLIC_URL: "http://localhost:3000/",
-  HTTP_SERVER_URL: "http://localhost:5000/",
+  HTTP_SERVER_URL: `https://${SERVER_URL}`,
   REFRESH_TOKEN: "refreshToken",
 };
 
